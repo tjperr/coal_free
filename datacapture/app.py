bfb294eecdfff3032c92e58be2d4540d6910cd34
tjperr <thomas_perrett@outlook.com>
2021-10-13 21:14:15 +0100

    tidy

--- datacapture/app.py
+++ datacapture/app.py
@@ -29,21 +29,9 @@ def lambda_handler(event, context):
         Return doc: https://docs.aws.amazon.com/apigateway/latest/developerguide/set-up-lambda-proxy-integrations.html
     """
 
-    # try:
-    #     ip = requests.get("http://checkip.amazonaws.com/")
-    # except requests.RequestException as e:
-    #     # Send some context about this error to Lambda Logs
-    #     print(e)
-
-    #     raise e
-
     print("working")
     print(event)
-    print("Lambda function ARN:", context.invoked_function_arn)
-    print("CloudWatch log stream name:", context.log_stream_name)
-    print("CloudWatch log group name:",  context.log_group_name)
-    print("Lambda Request ID:", context.aws_request_id)
-    print("Lambda function memory limits in MB:", context.memory_limit_in_mb)
+
     session = requests.session()
     response = session.get(
         f"https://api.bmreports.com/BMRS/FUELINSTHHCUR/v1",
