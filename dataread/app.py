f581aa5479f75e3963ec96f843d44f6d96055e73
tjperr <thomas_perrett@outlook.com>
2021-10-16 20:53:19 +0100

    dataread api

--- dataread/app.py
+++ dataread/app.py
@@ -8,32 +8,13 @@ import uuid
 
 
 def lambda_handler(event, context):
-    """Sample pure Lambda function
-
-    Parameters
-    ----------
-    event: dict, required
-        API Gateway Lambda Proxy Input Format
-
-        Event doc: https://docs.aws.amazon.com/apigateway/latest/developerguide/set-up-lambda-proxy-integrations.html#api-gateway-simple-proxy-for-lambda-input-format
-
-    context: object, required
-        Lambda Context runtime methods and attributes
-
-        Context doc: https://docs.aws.amazon.com/lambda/latest/dg/python-context-object.html
-
-    Returns
-    ------
-    API Gateway Lambda Proxy Output Format: dict
-
-        Return doc: https://docs.aws.amazon.com/apigateway/latest/developerguide/set-up-lambda-proxy-integrations.html
-    """
+    """Read all historic data from the s3 datastore"""
 
     print("working")
     
-    filename = f"data.json"
-    
     s3 = boto3.resource('s3')
-    object = s3.Object(os.environ['bucket_name'], filename)
+    object = s3.Object(os.environ['bucket_name'], "data.json")
+    data = json.loads(object.get()['Body'].read())
 
-    return {"statusCode": 200, "body": json.loads(object.get()['Body'].read())}
+    print(data)
+    return {"statusCode": 200, "body": str(data)}
